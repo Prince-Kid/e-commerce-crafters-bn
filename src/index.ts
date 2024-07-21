c90f9a7b8f804354edfa78845c2521d5e6bd0530
sevelinCA <ngabosevelin@gmail.com>
2024-07-21 23:06:33 +0200

    fix 2fa middelware

--- src/index.ts
+++ src/index.ts
@@ -59,13 +59,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use((req, res, next) => {
-  if (req.originalUrl === '/webhook') {
-    next();
-  } else {
-    express.json()(req, res, next);
-  }
-});
+app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
