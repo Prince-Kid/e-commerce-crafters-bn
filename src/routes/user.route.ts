6d90e72d9e0220a79f12ab0d17b14dfc36c798dd
sevelinCA <ngabosevelin@gmail.com>
2024-07-21 22:36:15 +0200

    updates

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,4 @@
-import { twoFAController } from "./../middleware/2fa.middleware";
+
 
 import express,{Request,Response} from "express";
 import {
@@ -30,7 +30,7 @@ route.post("/register", register);
 route.patch("/updateuser/:id", editUser);
 route.patch("/updatepassword/:id", updatePassword);
 route.delete("/deleteuser/:id", VerifyAccessToken, deleteUser);
-route.post("/login", twoFAController, login);
+route.post("/login", login);
 route.post("/addreview/:id", addReview);
 route.post("/addfeedback/:id", addFeedback);
 
