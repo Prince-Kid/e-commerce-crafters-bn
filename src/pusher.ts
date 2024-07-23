e9f141773b74f66d1d23c84f039df72327f36ccc
sevelinCA <ngabosevelin@gmail.com>
2024-07-24 01:06:13 +0200

    change pusher setup

--- src/pusher.ts
+++ src/pusher.ts
@@ -4,7 +4,7 @@ const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID as string,
   key: process.env.PUSHER_KEY as string,
   secret: process.env.PUSHER_SECRET as string,
-  cluster: "mt1",
+  cluster: process.env.PUSHER_CLUSTER as string,
 });
 
 export default pusher
